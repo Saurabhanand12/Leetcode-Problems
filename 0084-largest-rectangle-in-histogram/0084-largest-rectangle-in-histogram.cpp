ae6e63d05b838f703067967096b4dbe2e8061960
Saurabh <saurabhanand9811@gmail.com>
2026-09-16 10:10:37 +0530

    Time: 32 ms (21.75%), Space: 85.7 MB (33.45%) - LeetHub - Time: O(n), Space: O(n)

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,21 +2,52 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
+
+        vector<int> lefts(n, -1);
+        vector<int> rights(n, n);
+
         stack<int> st;
-        int maxA = 0;
-        for(int i=0;i<=heights.size();i++){
-            while(!st.empty() && (i == n || heights[st.top()] >= heights[i])){
-                int height = heights[st.top()];
+
+        // Previous Smaller Element
+        for(int i = 0; i < n; i++) {
+
+            while(!st.empty() && heights[st.top()] >= heights[i])
                 st.pop();
 
-                int width;
-                if(st.empty()) width = i;
-                else  width = i-st.top()-1;
+            if(!st.empty())
+                lefts[i] = st.top();
 
-                maxA = max(maxA,width* height);
-            }
-            if(i <n) st.push(i);
+            st.push(i);
         }
+
+        // Clear stack
+        while(!st.empty())
+            st.pop();
+
+        // Next Smaller Element
+        for(int i = n - 1; i >= 0; i--) {
+
+            while(!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            if(!st.empty())
+                rights[i] = st.top();
+
+            st.push(i);
+        }
+
+        // Calculate maximum area
+        long long maxA = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            long long width = rights[i] - lefts[i] - 1;
+
+            long long area = 1LL * width * heights[i];
+
+            maxA = max(maxA, area);
+        }
+
         return maxA;
     }
 };
